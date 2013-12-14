f1972a9d20e3836cf13b482e421c9082649e18a7
beepscore <support@beepscore.com>
2013-12-13 23:13:52 -0800

    In HRMViewController.h @import modules, add defines for bluetooth services and characteristics, adopt delegate protocols, add properties.

--- HeartMonitor/HRMViewController.h
+++ HeartMonitor/HRMViewController.h
@@ -7,7 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+@import CoreBluetooth;
+@import QuartzCore;
 
-@interface HRMViewController : UIViewController
+// https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx
+#define POLARH7_HRM_DEVICE_INFO_SERVICE_UUID @"180A"
+#define POLARH7_HRM_HEART_RATE_SERVICE_UUID @"180D"
+
+// https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicsHome.aspx
+#define POLARH7_HRM_MEASUREMENT_CHARACTERISTIC_UUID @"2A37"
+#define POLARH7_HRM_BODY_LOCATION_CHARACTERISTIC_UUID @"2A38"
+#define POLARH7_HRM_MANUFACTURER_NAME_CHARACTERISTIC_UUID @"2A29"
+
+@interface HRMViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate>
+
+@property (nonatomic, strong) CBCentralManager *centralManager;
+@property (nonatomic, strong) CBPeripheral *polarH7HRMPeripheral;
 
 @end
