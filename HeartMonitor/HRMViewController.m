3c174575e6e0605b0157018936e721918a416b50
beepscore <support@beepscore.com>
2013-12-13 23:35:54 -0800

    In HRMViewController.m add delegate method stubs.

--- HeartMonitor/HRMViewController.m
+++ HeartMonitor/HRMViewController.m
@@ -26,4 +26,53 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark CBCentralManagerDelegate
+
+// method called whenever you have successfully connected to the BLE peripheral
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+
+}
+
+// CBCentralManagerDelegate - This is called with the CBPeripheral class as its main input parameter.
+// This contains most of the information there is to know about a BLE peripheral.
+- (void)centralManager:(CBCentralManager *)central
+ didDiscoverPeripheral:(CBPeripheral *)peripheral
+     advertisementData:(NSDictionary *)advertisementData
+                  RSSI:(NSNumber *)RSSI
+{
+
+}
+
+// method called whenever the device state changes
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+
+}
+
+#pragma mark CBPeripheralDelegate
+
+// Invoked when you discover the peripheral's available services.
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
+{
+
+}
+
+// invoked when you discover the characteristics of a specified service
+- (void)peripheral:(CBPeripheral *)peripheral
+didDiscoverCharacteristicsForService:(CBService *)service
+             error:(NSError *)error
+{
+
+}
+
+// invoked when you retrieve a specified characteristic's value,
+// or when the peripheral device notifies your app that the charactteristic's value has changed.
+- (void)peripheral:(CBPeripheral *)peripheral
+didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic
+             error:(NSError *)error
+{
+
+}
+
 @end
